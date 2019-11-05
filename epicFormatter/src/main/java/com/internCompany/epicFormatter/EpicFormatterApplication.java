795ce33875f9df2f2e61248d0b1beaca3a173d4c
student <student@lab-45-2.cs.put.poznan.pl>
2019-11-05 11:08:27 +0100

    Initial structure of our project upload

---
+++ epicFormatter/src/main/java/com/internCompany/epicFormatter/EpicFormatterApplication.java
@@ -0,0 +1,13 @@
+package com.internCompany.epicFormatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicFormatterApplication.class, args);
+	}
+
+}
---
+++ epicFormatter/src/test/java/com/internCompany/epicFormatter/EpicFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internCompany.epicFormatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
