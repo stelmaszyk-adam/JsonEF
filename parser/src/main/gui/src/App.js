0dec3be2d96c905d8610322a5c022ed56177a879
Vladvance <hl.vladyslav@gmail.com>
2020-01-10 11:51:43 +0100

    completed interface

--- parser/src/main/gui/src/App.js
+++ parser/src/main/gui/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Layout from './components/layout';
+import Home from './Home.js'
+import About from './About.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <Layout>
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+    </Switch>
+    </Layout>
+    </Router>
+  )
 }
 
 export default App;
