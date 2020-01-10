29c05a477bb554c5c7a3b1ed0ddb5e867b1e3770
Agere <bmateusz1998@gmail.com>
2020-01-10 21:28:20 +0100

    Fix controller path

--- parser/src/main/java/json/tools/FormatterApplication.java
+++ parser/src/main/java/json/tools/FormatterApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"json.formatter.rest"})
+@SpringBootApplication(scanBasePackages = {"json.tools.rest"})
 public class FormatterApplication {
 
 	public static void main(String[] args) {
